e14624dcd1643b7740111f693e8cf058189a27f0
Vladimir Balakin <vladimirbalakin933@gmail.com>
2023-05-17 22:51:03 +0300

    Логирование в эластик

--- src/main/java/com/nekromant/telegram/controller/PageController.java
+++ src/main/java/com/nekromant/telegram/controller/PageController.java
@@ -1,11 +1,13 @@
 package com.nekromant.telegram.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@Slf4j
 public class PageController {
 
     @GetMapping("/incomingReviewWidget")
