a0ce1247df5cd6e025b3fb4fe1b1e37dea4cd5d9
Vladislav812 <bolotin_vlad@list.ru>
2024-06-18 20:27:19 +0300

    trigger cd check

--- src/main/java/com/nekromant/telegram/service/ClientPaymentRequestService.java
+++ src/main/java/com/nekromant/telegram/service/ClientPaymentRequestService.java
@@ -4,6 +4,7 @@ import com.nekromant.telegram.contants.ServiceType;
 import com.nekromant.telegram.model.PaymentDetails;
 
 public interface ClientPaymentRequestService {
+    
     public void notifyMentor(PaymentDetails paymentDetails);
 
     public void rejectApplication(PaymentDetails paymentDetails);
