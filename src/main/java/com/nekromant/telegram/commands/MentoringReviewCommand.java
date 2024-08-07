3178420b78831d27870beaadb610171af0dd0940
Anatolii Marandyuk <shamaneeel881@gmail.com>
2024-08-07 14:47:42 +0300

    fix logging

--- src/main/java/com/nekromant/telegram/commands/MentoringReviewCommand.java
+++ src/main/java/com/nekromant/telegram/commands/MentoringReviewCommand.java
@@ -1,13 +1,14 @@
 package com.nekromant.telegram.commands;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-
+@Slf4j
 public abstract class MentoringReviewCommand extends BotCommand {
 
     public MentoringReviewCommand(String commandIdentifier, String description) {
@@ -15,8 +16,8 @@ public abstract class MentoringReviewCommand extends BotCommand {
     }
 
     public void execute(AbsSender sender, SendMessage message, User user) {
-        System.out.println(this.getDescription() + " , пользователь - " + user.getUserName());
-        System.out.println("output: \n" + message.getText() + "\n");
+        log.debug(this.getDescription() + " , пользователь - " + user.getUserName());
+        log.debug("output: \n" + message.getText() + "\n");
         try {
             sender.execute(message);
         } catch (TelegramApiException e) {
