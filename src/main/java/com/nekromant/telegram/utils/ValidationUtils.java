d6ad8d0d122570ff389c4f74f1e2f93a71fd42c9
Anton Kovalyov <kovalyov.af7@gmail.com>
2024-08-20 18:50:40 +0700

    Add hours validation to report command

--- src/main/java/com/nekromant/telegram/utils/ValidationUtils.java
+++ src/main/java/com/nekromant/telegram/utils/ValidationUtils.java
@@ -6,7 +6,11 @@ public class ValidationUtils {
 
     public static void validateArguments(String[] strings) {
         if (strings == null || strings.length == 0) {
-            throw new InvalidParameterException();
+            throw new InvalidParameterException("Wrong arguments count");
+        }
+        int i = Integer.parseInt(strings[1]);
+        if (i < 0 || i > 24) {
+            throw new InvalidParameterException("Неверное значение часов — должно быть от 0 до 24");
         }
     }
 }
