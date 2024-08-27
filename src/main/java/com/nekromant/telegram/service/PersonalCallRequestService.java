e864d7675cc11f9dbe6f9c6331f8800786290ed4
Alexander Katsur <alexander_katzur@mail.ru>
2024-08-27 19:45:32 +0700

    Отработал по комментариям. Изменения в PersonalCallRequestService.java, docker-compose.yml и deploy.yml

--- src/main/java/com/nekromant/telegram/service/PersonalCallRequestService.java
+++ src/main/java/com/nekromant/telegram/service/PersonalCallRequestService.java
@@ -7,17 +7,16 @@ import com.nekromant.telegram.contants.ServiceType;
 import com.nekromant.telegram.model.PaymentDetails;
 import com.nekromant.telegram.model.PersonalCallRequest;
 import com.nekromant.telegram.repository.PersonalCallRequestRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Optional;
 
 import static com.nekromant.telegram.contants.MessageContants.PERSONAL_CALL_DESCRIPTION;
 import static com.nekromant.telegram.contants.MessageContants.RESPONSE_FOR_PERSONAL_CALL;
 
-@Slf4j
 @Service
 public class PersonalCallRequestService extends ClientPaymentRequestServiceCommon implements ClientPaymentRequestService {
     @Autowired
@@ -27,12 +26,12 @@ public class PersonalCallRequestService extends ClientPaymentRequestServiceCommo
     @Autowired
     private PriceProperties priceProperties;
 
-    public ResponseEntity save (Map callData) {
+    public ResponseEntity save(Map callData) {
         PersonalCallRequest personalCallRequest = PersonalCallRequest.builder()
                 .tgName(callData.get("TG-NAME").toString())
                 .customerPhone(callData.get("PHONE").toString())
                 .build();
-        String promocodeId = callData.get("CALL-PROMOCODE-ID") == null ? null : callData.get("CALL-PROMOCODE-ID").toString();
+        String promocodeId = Optional.ofNullable(callData.get("CALL-PROMOCODE-ID")).map(Object::toString).orElse(null);
         ChequeDTO chequeDTO = new ChequeDTO(lifePayProperties.getLogin(),
                 lifePayProperties.getApikey(),
                 calculatePriceWithOptionalDiscount(priceProperties.getPersonalCall(), promocodeId),
