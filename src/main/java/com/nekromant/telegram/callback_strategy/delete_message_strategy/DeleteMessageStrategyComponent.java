8598dd1260f8d20274ca1fd3dfa524b518aaf933
Anton Kovalyov <kovalyov.af7@gmail.com>
2024-09-26 18:46:55 +0700

    Make DeleteMessageStrategyComponent of prototype scope

--- src/main/java/com/nekromant/telegram/callback_strategy/delete_message_strategy/DeleteMessageStrategyComponent.java
+++ src/main/java/com/nekromant/telegram/callback_strategy/delete_message_strategy/DeleteMessageStrategyComponent.java
@@ -2,11 +2,12 @@ package com.nekromant.telegram.callback_strategy.delete_message_strategy;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
-@Component
+@Component(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class DeleteMessageStrategyComponent {
     private DeleteMessageStrategy deleteMessageStrategy;
 }
