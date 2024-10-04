30eb3aed57dda44fb8456cce7f9c5e3ccdb90083
Anton Kovalyov <kovalyov.af7@gmail.com>
2024-10-04 17:41:08 +0700

    Add javadocs to ChatMessage entity

--- src/main/java/com/nekromant/telegram/model/ChatMessage.java
+++ src/main/java/com/nekromant/telegram/model/ChatMessage.java
@@ -5,6 +5,9 @@ import lombok.*;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+/**
+ * Представляет собой связь между сообщениями чата и отчётом.
+ */
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,10 +19,22 @@ public class ChatMessage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    /**
+     * ID сообщения пользователя, в котором была вызвана команда.
+     */
     @NotNull
     private Integer userMessageId;
+    /**
+     * ID сообщения бота в чате юзера с отправленным отчётом.
+     */
     private Integer userChatBotMessageId;
+    /**
+     * ID отчёта в БД, который был закреплён за сообщением.
+     */
     @OneToOne
     private Report report;
+    /**
+     * ID сообщения бота в чате отчётов с отправленным отчётом.
+     */
     private Integer reportChatBotMessageId;
 }
