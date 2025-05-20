261d393962a362214f3a6efdc7ad976f7c8f89b0
m004ka <ysupov959@gmail.com>
2025-05-20 04:33:25 +0300

    Команды на отключение и включение уведомлений

---
+++ src/main/java/com/nekromant/telegram/commands/notification/DisablePayNotification.java
@@ -0,0 +1,57 @@
+package com.nekromant.telegram.commands.notification;
+
+import com.nekromant.telegram.commands.MentoringReviewCommand;
+import com.nekromant.telegram.contants.MessageContants;
+import com.nekromant.telegram.model.Mentor;
+import com.nekromant.telegram.repository.MentorRepository;
+import com.nekromant.telegram.service.NotificationService;
+import com.nekromant.telegram.utils.ValidationUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.bots.AbsSender;
+
+import java.security.InvalidParameterException;
+
+import static com.nekromant.telegram.contants.Command.DISABLE_NOTIFICATION;
+
+@Component
+public class DisablePayNotification extends MentoringReviewCommand {
+
+    @Autowired
+    private MentorRepository mentorRepository;
+    @Autowired
+    private NotificationService notificationService;
+
+    public DisablePayNotification() {
+        super(DISABLE_NOTIFICATION.getAlias(), DISABLE_NOTIFICATION.getDescription());
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+        SendMessage message = new SendMessage();
+        String chatId = chat.getId().toString();
+        message.setChatId(chatId);
+        ValidationUtils.validateArgumentsNumber(strings);
+
+        try {
+            Mentor mentor = mentorRepository.findMentorByMentorInfo_ChatId(user.getId());
+            if (mentor == null) {
+                message.setText("Ты не ментор");
+                execute(absSender, message, user);
+            } else {
+                notificationService.disablePayNotification(strings);
+                message.setText(MessageContants.SUCCESS_SET_NOTIFICATION);
+                execute(absSender, message, user);
+            }
+        } catch (InvalidParameterException e) {
+            message.setText("Укажи аргументы");
+            execute(absSender, message, user);
+        } catch (Exception e) {
+            message.setText(MessageContants.FAILED_SET_NOTIFICATION);
+            execute(absSender, message, user);
+        }
+    }
+}
